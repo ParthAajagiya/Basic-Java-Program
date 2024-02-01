1e3398ca2efba9561ac8530acfed12294a41df3b
ParthAajagiya <techparth072@gmail.com>
2024-02-01 12:06:30 +0530

    First Commit

---
+++ src/DateAndTime/DateAndTimeEX2.java
@@ -0,0 +1,26 @@
+package DateAndTime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Date;
+
+public class DateAndTimeEX2
+{
+    public static void main(String s[])
+    {
+//        To take local date
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld);
+//To print date
+        LocalDate localDate = LocalDate.of(2024, Month.JANUARY, 14);
+        System.out.println(localDate);
+
+        LocalDate localDate1 = LocalDate.ofYearDay(2024, 325);//give date
+        System.out.println(localDate1);
+
+        int hour = LocalTime.now().getHour();
+        System.out.println(hour);
+
+    }
+}
---
+++ src/DateAndTime/DateEx.java
@@ -0,0 +1,17 @@
+package DateAndTime;
+
+import java.util.Date;
+
+public class DateEx
+{
+    public static void main(String s[])
+    {
+        Date d = new Date();
+        System.out.println(d.getDate());
+        System.out.println(d.getDay());
+        System.out.println(d.getTime());
+
+        System.out.println(d.getYear()+1900); //It counts Year from 1900 so we have to add+1900
+        System.out.println(d.getMonth()+1); //Count month from 0
+    }
+}
